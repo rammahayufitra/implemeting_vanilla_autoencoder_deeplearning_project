51a942533b636694eae620a67c8d9affa23eaf5a
ramma.fitra <rammafitracyt@gmail.com>
2022-09-09 16:13:52 +0700

    [add]commit3

--- src/inference.py
+++ src/inference.py
@@ -9,9 +9,9 @@ from models import AutoEncoder
 device = 'cuda:0'
 
 
-model = AutoEncoder(3).to(device)
+# model = AutoEncoder(3).to(device)
 model = torch.load('../public/models/model.pt', map_location=device)
-checkpoint = torch.load('../public/weights/weight.pt', map_location=device)
+checkpoint = torch.load('../public/weights/weights.pt', map_location=device)
 model.load_state_dict(checkpoint)
 model.to(device)
 model.eval()
@@ -26,24 +26,14 @@ vid = cv2.VideoCapture("https://192.168.1.12:8080/video")
 
 value = []
 while(True):
-    ret, image = vid.read()
-    image = cv2.resize(image, (32,32), interpolation = cv2.INTER_AREA)
-    # image = Image.fromarray(image).convert('L')
+    ret, frame = vid.read()
+    frame = cv2.resize(frame, (28,28), interpolation = cv2.INTER_AREA)
+    image = Image.fromarray(frame).convert('L')
     image = transform(image)
-    # image = image.unsqueeze(0).to(device)
-    outputs = model(image)[0]
+    outputs = model(image)[0][0].cpu().detach().numpy()
     print(outputs)
-    # label = np.array(outputs.detach()).argmax()   
-    # value.append(label)
-    # font = cv2.FONT_HERSHEY_SIMPLEX
-    # org = (00, 185)
-    # fontScale = 1
-    # color = (0, 0, 255)
-    # thickness = 2
-    # image = cv2.putText(image, str(label), org, font, fontScale, 
-    #                 color, thickness, cv2.LINE_AA, False)
-    # cv2.imshow('frame1', frame1)
-    # cv2.imshow('frame', image)
+    cv2.imshow('input', frame)
+    cv2.imshow('output', outputs)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
